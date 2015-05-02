f1813ba6d5d878327e04f26484367a81d7d27058
Daniel Cloud <daniel@danielcloud.org>
2015-05-02 19:03:26 -0400

    Add many more little tests.

--- marvelous/tests/test_marvel.py
+++ marvelous/tests/test_marvel.py
@@ -12,6 +12,14 @@ class TestMarvel(unittest.TestCase):
 
     def setUp(self):
         self.api = Marvel(public_key=MARVEL_API_PUBLIC_KEY, private_key=MARVEL_API_PRIVATE_KEY)
+        self.wolverine = 1009718
+        self.doop = 1009279
+        self.bendis = 24
+        self.story = 15
+
+    def test_api_session_configuration(self):
+        self.assertIsNotNone(self.api._session)
+        self.assertEqual(self.api._session.headers['Accept-Encoding'], 'gzip')
 
     def test_needs_public_and_private_keys(self):
         unauthapi = Marvel()
@@ -19,7 +27,7 @@ class TestMarvel(unittest.TestCase):
             unauthapi.comics()
 
     def test_character_by_id(self):
-        result = self.api.character(1011334)
+        result = self.api.character(self.wolverine)
         self.assertIsInstance(result, dict)
 
     def test_character_missing_id(self):
@@ -36,6 +44,26 @@ class TestMarvel(unittest.TestCase):
         self.assertIsInstance(results, list)
         self.assertEqual(len(results), 1)
 
+    def test_characters_comics(self):
+        results = self.api.characters_comics(self.doop)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_characters_events(self):
+        results = self.api.characters_events(self.doop)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_characters_series(self):
+        results = self.api.characters_series(self.doop)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_characters_stories(self):
+        results = self.api.characters_stories(self.doop)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
     def test_story_by_id(self):
         result = self.api.story(3)
         self.assertIsInstance(result, dict)
@@ -43,3 +71,52 @@ class TestMarvel(unittest.TestCase):
     def test_story_missing_id(self):
         with self.assertRaises(TypeError):
             self.api.story()
+
+    def test_stories_with_characters(self):
+        results = self.api.stories(characters=[self.wolverine, self.doop])
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_stories_characters(self):
+        results = self.api.stories_characters(self.story)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_stories_comics(self):
+        results = self.api.stories_comics(self.story)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_stories_creators(self):
+        results = self.api.stories_creators(self.story)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_stories_events(self):
+        results = self.api.stories_events(self.story)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_stories_series(self):
+        results = self.api.stories_series(self.story)
+        self.assertIsInstance(results, list)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_creator_by_id(self):
+        result = self.api.creator(self.bendis)
+        self.assertIsInstance(result, dict)
+
+    def test_creators_comics(self):
+        results = self.api.creators_comics(self.bendis)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_creators_events(self):
+        results = self.api.creators_events(self.bendis)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_creators_series(self):
+        results = self.api.creators_series(self.bendis)
+        self.assertEqual(len(results), results._meta['data']['count'])
+
+    def test_creators_stories(self):
+        results = self.api.creators_stories(self.bendis)
+        self.assertEqual(len(results), results._meta['data']['count'])
